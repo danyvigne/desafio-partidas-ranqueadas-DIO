2e3dd19308a4f28afeda0551fa909e7620b4c773
danyvigne <danyvigne2912@gmail.com>
2023-12-30 10:17:50 -0300

    atualização de arquivos do projeto

--- desafio.js
+++ desafio.js
@@ -1,46 +1,45 @@
 let vitorias = 30
 let derrotas = 15
-let saldoVitorias = saldoVit
+let saldoVitorias = saldoRank(vitorias, derrotas)
 
-function saldoRank(){
+function saldoRank(vitorias, derrotas){
     let saldoVit = vitorias - derrotas
     return saldoVit
-
 }
 
 switch (true) {
     case (saldoVitorias <= 10):
         nivel = "Ferro"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
     break
 
-    case (saldoVitorias >= 11 && xp <= 20):
+    case (saldoVitorias >= 11 && saldoVitorias <= 20):
         nivel = "bronze"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
     break    
 
-    case (saldoVitorias >= 21 && xp <= 50):
+    case (saldoVitorias >= 21 && saldoVitorias <= 50):
         nivel = "Prata"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
     break   
     
-    case (saldoVitorias >= 51 && xp <= 80):
+    case (saldoVitorias >= 51 && saldoVitorias <= 80):
         nivel = "Ouro"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
     break    
 
-    case (saldoVitorias >= 81 && xp <= 90):
+    case (saldoVitorias >= 81 && saldoVitorias <= 90):
         nivel = "Diamante"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
     break    
 
-    case (saldoVitorias >= 91 && xp <= 100):
+    case (saldoVitorias >= 91 && saldoVitorias <= 100):
         nivel = "Lendário"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
     break    
 
     default:
         nivel = "Imortal"
-        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);   
+        console.log(`O Herói tem o saldo de ${saldoVitorias} está no nível de ${nivel}`);   
 
 }
